5a213c8482ebe37a1da3aab10aef028108e81946
kennymartinez19k <kennymartinez19k@gmail.com>
2023-12-05 13:46:25 -0400

    save changes

--- SQLQuery3.sql
+++ SQLQuery3.sql
@@ -1,10 +1,11 @@
---Crea una base de datos llamada hecl lavanderia
+create database HECL_LANGUAGE_INSTITUTE;
 
-create database HECL_LANGUAGE_INSTITUTE
 
-use HECL_LANGUAGE_INSTITUTE
+use HECL_LANGUAGE_INSTITUTE;
+
 
 create table usuarios (
+	id INT IDENTITY(1,1) PRIMARY KEY,
     nombre varchar(100),
     apellido varchar(100),
     telefono int,
@@ -17,32 +18,39 @@ create table usuarios (
 	curso varchar(100)
 );
 
+
 create table cursos (
-    id int primary key NOT NULL,
+    id INT IDENTITY(1,1) PRIMARY KEY,
     nombre varchar(100),
 	duracion_horas Int
 );
 
+
 CREATE TABLE sesiones (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT IDENTITY(1,1) PRIMARY KEY,
     id_curso INT,
     sesion_numero INT,
 	FOREIGN KEY (id_curso) REFERENCES cursos(id)
 );
 
-CREATE TABLE materias (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    id_profesor INT,
-    sesion_numero INT,
-	FOREIGN KEY (id_profesor) REFERENCES profesores(id)
-);
-
 
 CREATE TABLE profesores (
-    id int primary key NOT NULL,
+    id INT IDENTITY(1,1) PRIMARY KEY,
     nombre varchar(100),
 	apellido varchar(100),
     telefono int,
     email varchar(100),
     direccion varchar(100),
 );
+
+go
+
+CREATE TABLE materias (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    id_profesor INT,
+    sesion_numero INT,
+	FOREIGN KEY (id_profesor) REFERENCES profesores(id)
+);
+
+
+
